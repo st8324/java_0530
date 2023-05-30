6401695b84d64bd6b675b63c8808d9742c9f40ec
st8324 <stajun@kut.ac.kr>
2023-05-30 12:45:43 +0900

    자바 : 프로젝트 생성 및 자바 샘플 코드 추가

---
+++ java_jik/src/day1/sample/JavaSample.java
@@ -0,0 +1,17 @@
+package day1.sample;
+
+public class JavaSample {
+
+	// 한줄 주석 : 코드에 영향이 없는 설명 문구
+	/* 여러줄 주석
+	*/
+	/**
+	 * 메소드를 설명하는 주석 : 메소드를 배울 때 다시 설명
+	 * @param args
+	 */
+	// main : 프로그램이 실행되기 위해 필요한 것(메소드)
+	public static void main(String[]args) {
+		//콘솔창에 원하는 문구를 출력하는 기능을 가진 것(메소드)
+		System.out.println("Hello Java!");
+	}
+}
