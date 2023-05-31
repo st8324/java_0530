8471a092e7aedb2cccc2642151a7cb85e4e78ab7
st8324 <stajun@kut.ac.kr>
2023-05-31 12:41:44 +0900

    자바 : 변수 선언 예제

---
+++ java_jik/src/day2/variable/VariableEx.java
@@ -0,0 +1,13 @@
+package day2.variable;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		//e는 10을 의미, e-3은 10의 -3제곱을 의미. 결과는 double
+		double num2 = 3e-3;//3/1000 == 0.003
+		System.out.println(num2);
+
+	}
+
+}
