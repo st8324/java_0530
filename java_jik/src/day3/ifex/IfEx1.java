0afb0166bf470b2bb8f6a0c0cc8aa2e04073ae12
st8324 <stajun@kut.ac.kr>
2023-06-01 14:56:34 +0900

    자바 : if문 예제 - 0 판별 예제

---
+++ java_jik/src/day3/ifex/IfEx1.java
@@ -0,0 +1,22 @@
+package day3.ifex;
+
+public class IfEx1 {
+
+	public static void main(String[] args) {
+		//정수 num가 0이면 0입니다라고 출력하는 코드를 작성하세요.
+		//정수 num가 0이다
+		//정수 num가 0과 같다
+		int num = 0;
+		if(num == 0) {
+			//0입니다라고 출력;
+			System.out.println("0 is 0");
+		}
+		//정수 num가 0이 아니면 num is not 0라고 출력하는 코드를 작성하세요.
+		//num가 0이 아니다
+		//num가 0과 같지 않다
+		if(num != 0) {
+			System.out.println(num + " is not 0");
+		}
+	}
+
+}
