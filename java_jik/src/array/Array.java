5ce8acf217f08b56f8e3d448403601ea07c726c7
st8324 <stajun@kut.ac.kr>
2023-06-16 12:36:46 +0900

    자바 : Array 클래스에 정렬 추가

--- java_jik/src/array/Array.java
+++ java_jik/src/array/Array.java
@@ -38,7 +38,7 @@ public class Array {
 		if(max < min) {
 			int tmp = max;
 			max = min;
-			min = max;
+			min = tmp;
 		}
 		
 		//배열이 생성되어 있지 않으면(배열이 null이면) 메서드 종료
@@ -89,5 +89,32 @@ public class Array {
 		return false;
 	}
 	
-	
+	/**정수형 배열이 주어지면 오름차순으로 버블 정렬하는 메서드
+	 * 매개변수 : 정수형 배열 => int arr[]
+	 * 리턴타입 : 없음 => void
+	 * 메서드명 : sort
+	 * */
+	public static void sort(int arr[]) {
+		if(arr == null) {
+			return;
+		}
+		for(int i = 0; i<arr.length-1; i++) {
+			for(int j = 0; j<arr.length-1-i; j++) {
+				if(arr[j] > arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
