bc6ee51e3c3e5207e407e5cfdec04dfd7604b7ca
st8324 <stajun@kut.ac.kr>
2023-08-04 16:50:12 +0900

    DB : 개인쇼핑몰사이트 - 샘플데이터 추가 쿼리

---
+++ db/15.개인쇼핑몰사이트_샘플데이터 추가 쿼리.sql
@@ -0,0 +1,15 @@
+USE SHOPPINGMALL;
+
+-- 회원아이디가 abc123, 비번이 1234, 번호가 011-1234-5678, 이름이 홍길동 
+-- 주소가 서울시 강남구 KH빌딩, 주소 상세가 6층 361반, 우편번호가 01234인 회원이 가입을 했을 때 쿼리 작성
+-- 권한은 ADMIN, USER 중 하나 
+INSERT INTO MEMBER(ME_ID, ME_PW, ME_PHONE, ME_ADDR, ME_ADDR_DETAIL, ME_POST, ME_AUTHORITY, ME_NAME)
+	VALUES('abc123','1234','011-1234-5678','서울시 강남구 KH빌딩', '6층 361반', '01234', 'USER', '홍길동');
+
+INSERT INTO MEMBER(ME_ID, ME_PW, ME_PHONE, ME_ADDR, ME_ADDR_DETAIL, ME_POST, ME_AUTHORITY, ME_NAME)
+	VALUES('admin','admin','011-1111-1111','서울시 강북구 KH빌딩', '3층 101호', '01231', 'ADMIN', '임꺽정');
+INSERT INTO MEMBER(ME_ID, ME_PW, ME_PHONE, ME_ADDR, ME_ADDR_DETAIL, ME_POST, ME_AUTHORITY, ME_NAME)
+	VALUES('qwe123','qwe1234','011-9999-5678','서울시 강남구 KH빌딩', '1층 161반', '01234', 'USER', '고길동');
+
+-- 제품코드가 ABC001, 제품명이 KH무선마우스, 제품설명이 KH무선마우스입니다., 가격이 15000원
+-- 종류가 무선 / 유선인 제품을 등록하는 쿼리를 작성 
