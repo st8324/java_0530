e6e61f921f9d1a25fd6723724338d8c9410373c0
st8324 <stajun@kut.ac.kr>
2023-09-06 11:57:01 +0900

    SPRING : ajax 샘플 코드

--- spring projects/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring projects/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,9 +1,16 @@
 package kr.kh.spring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.kh.spring.vo.MemberVO;
 
 @Controller
 public class HomeController {
@@ -20,5 +27,19 @@ public class HomeController {
 		System.out.println("나이 : " + age);
 		return "test1";
 	}
-	
+	@ResponseBody
+	@PostMapping("/ajax/test")
+	public Map<String, Object> ajaxTest(@RequestBody MemberVO member){
+		Map<String, Object> map = new HashMap<String, Object>();
+		System.out.println(member);
+		map.put("name", "홍길동");
+		return map;
+	}
 }
+
+
+
+
+
+
+
--- spring projects/spring/src/main/webapp/WEB-INF/views/main/home.jsp
+++ spring projects/spring/src/main/webapp/WEB-INF/views/main/home.jsp
@@ -7,5 +7,24 @@
 </head>
 <body>
 	<h1>메인</h1>
+	
+	<script type="text/javascript">
+	let data = {
+		me_id : 'abc',
+		me_pw : 'test'
+	};
+	$.ajax({
+		async : false, /*비동기 : false => 동기화*/
+		type : 'post', /*전송 방식 : get/post*/
+		url : '<c:url value="/ajax/test"/>', /*데이터를 보낼 url*/
+		data : JSON.stringify(data), /*보낼 데이터, 보통 객체나 json으로 보냄*/
+		contentType : "application/json; charset=UTF-8", /*서버로 보낼 데이터의 타입 */
+		dataType : "json", /* 서버에서 화면으로 보낸 데이터의 타입 */
+		success : function(data){ /* ajax가 성공하면 실행될 메서드로 서버에서 보낸 데이터를 매개변수에 저장 */
+			console.log(data);
+		}
+	});
+	
+	</script>
 </body>
 </html>
